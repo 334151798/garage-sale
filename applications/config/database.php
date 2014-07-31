3966630978e0a2e18dba07913c2df8f3730a6f43
Arbalest <334151798@qq.com>
2014-07-31 18:24:12 +0800

    1

--- applications/config/database.php
+++ applications/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'ZHANGWEI';
-$db['default']['database'] = 'tohaodian';
+$db['default']['password'] = 'wangwang';
+$db['default']['database'] = 'garage-sale';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
